9b906b433c3b88e71fa29536e818c5057cc5bea3
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-03-23 14:54:03 +0000

    mobile detection testing

--- src/js/app/mobileBrowser.js
+++ src/js/app/mobileBrowser.js
@@ -1,14 +1,15 @@
 // import MobileDetect from ('mobile-detect');
 var MobileDetect = require('mobile-detect');
 
-
-
 export default function mobileCheck() {
 
   var md = new MobileDetect(window.navigator.userAgent);
 
-  window.alert( md.mobile() );
-  window.alert( md.phone() );
-  window.alert( md.tablet() ); 
+  if (md.phone() === null && md.tablet() === null) {
+    window.alert('working');
+  } else {
+    window.alert('not working');
+  }
+
 
 }
